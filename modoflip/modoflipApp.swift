bcd3685cf03e5c063d245a722c9e633906e2615a
Sandor Orszagh Bihari <sandor.orszagh@profession.hu>
2025-09-24 09:46:36 +0200

    Initial Commit

---
+++ modoflip/modoflipApp.swift
@@ -0,0 +1,17 @@
+//
+//  modoflipApp.swift
+//  modoflip
+//
+//  Created by Orszagh Bihari Sandor  on 2025. 09. 24..
+//
+
+import SwiftUI
+
+@main
+struct modoflipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
